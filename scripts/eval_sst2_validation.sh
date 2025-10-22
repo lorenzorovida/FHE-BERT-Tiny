2ad10baeacafe91464a39c24a499c5e5ceae91dd
narger <52449688+narger-ef@users.noreply.github.com>
2025-10-22 18:45:56 +0200

    Update eval_sst2_validation.sh

--- scripts/eval_sst2_validation.sh
+++ scripts/eval_sst2_validation.sh
@@ -8,5 +8,6 @@ output_file="results.txt"
 
 while IFS= read -r line; do
     # Run your program on each line and append the output to the results file
-    ../build/FHE-BERT-Tiny "$line" --plain>> "$output_file"
+    echo "I am at: $line"
+    ../build/FHE-BERT-Tiny "$line" --plain >> | tee -a "$output_file"
 done < "$input_file"
